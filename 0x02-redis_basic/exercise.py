935f87a52ff1807411480b545e1df069f528dab4
Matrix30 <Matrixboy30@gmail.com>
2024-05-02 00:13:06 +0100

    finish task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,14 +15,16 @@ class Cache:
     """
 
     def __init__(self) -> None:
-        """hellow"""
+        """
+        creates a new redis cache object
+        that would be used to read and write to redis store
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         method takes data argument and stores in redis store and
-        returns string key to that data
         """
         redis_key = str(uuid.uuid4())
         self._redis.set(redis_key, data)
