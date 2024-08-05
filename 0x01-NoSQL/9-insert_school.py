0effdd5ed976eef474c54c31ee09b8341e6f568d
Matrix30 <matrixboy30@gmail.com>
2024-08-06 00:24:52 +0100

    finish task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,5 +5,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """inserts a new document to a collection"""
-    mongo_collection.insert(*)
-    
+    data = mongo_collection.insert_one(kwargs)
+    return data.inserted_id if data else None
