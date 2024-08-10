4a5a81e03da009a9b1cbc7561fb4dda6f4714e9b
Matrix30 <matrixboy30@gmail.com>
2024-08-11 00:11:52 +0100

    finish task 11 schools by topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,4 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """get schools by a specific topic"""
-    return [mongo_collection.find({"topic": })]
+    return mongo_collection.find({"topic": topic})
